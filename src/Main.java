033e6daf52bb29187b898180d767e47fdc626aa9
Clio <estclio09@gmail.com>
2018-02-13 22:58:24 +0200

    1 commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ArrayList listOfCommands = new ArrayList();
+        String command = "Список доступных команд - нажмите 0";
+        String command1 = "Список квартир - нажмите 1";
+        String command2 = "Список квартир на этаже - нажмите 2";
+        listOfCommands.add(command);
+        listOfCommands.add(command1);
+        listOfCommands.add(command2);
+        for (int i = 0; i < listOfCommands.size(); i++) {
+            System.out.println(listOfCommands.get(i));
+        }
     }
 }
