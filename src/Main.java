0c5f8134d05077a1cbc31c78ae6ba53064d859c9
Clio <estclio09@gmail.com>
2018-02-14 22:05:14 +0200

    2 commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,23 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        printListOfCommands();
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextInt();
+    }
+
+    public static void printListOfCommands() {
+        System.out.println("Cписок доступных команд: ");
         ArrayList listOfCommands = new ArrayList();
-        String command = "Список доступных команд - нажмите 0";
-        String command1 = "Список квартир - нажмите 1";
-        String command2 = "Список квартир на этаже - нажмите 2";
-        listOfCommands.add(command);
-        listOfCommands.add(command1);
-        listOfCommands.add(command2);
+        listOfCommands.add("Список доступных команд - нажмите 0");
+        listOfCommands.add("Список квартир - нажмите 1");
+        listOfCommands.add("Список квартир на этаже - нажмите 2");
         for (int i = 0; i < listOfCommands.size(); i++) {
             System.out.println(listOfCommands.get(i));
         }
+        System.out.println("Введите цифру: ");
     }
 }
