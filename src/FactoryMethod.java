b483173d932c610f4e5dd4437465a6bbf45f0fa3
Clio <estclio09@gmail.com>
2018-02-27 22:10:40 +0200

    попереджаю,помилка э в покупці квартири,лист знову не змінюється....

--- src/FactoryMethod.java
+++ src/FactoryMethod.java
@@ -1,23 +1,51 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class FactoryMethod {
-    public ChooseCommand getCommand (Object o){
-        ChooseCommand chooseCommand = null;
-        if (o instanceof BuyTheFlat) {
-            chooseCommand = new BuyTheFlat();
-        } else if (o instanceof PrintListOfCommands) {
-            chooseCommand = new PrintListOfCommands();
-        } else if (o instanceof PrintListOfFlats){
-            chooseCommand = new PrintListOfFlats();
-        } else if (o instanceof ShowAvailableFlats){
-            chooseCommand = new ShowAvailableFlats();
-        } else if (o instanceof ShownFlatConsistent){
-            chooseCommand = new ShownFlatConsistent();
-        } else if (o instanceof ShowNotAvailableFlats){
-            chooseCommand = new ShowNotAvailableFlats();
-        } else if (o instanceof ShownRange){
-            chooseCommand = new ShownRange();
+    public static void getCommand(int number) {
+        Novostroika novostroika = new Novostroika(new ArrayList<>());
+        ArrayList list = novostroika.list;
+        Scanner scanner = new Scanner(System.in);
+        ;
+        switch (number) {
+
+            case 0:
+                PrintListOfCommands printListOfCommands = new PrintListOfCommands();
+                printListOfCommands.mainCommand2(list);
+                break;
+            case 1:
+                PrintListOfFlats printListOfFlats = new PrintListOfFlats();
+                printListOfFlats.mainCommand(list);
+                break;
+            case 6:
+                ShownFlatConsistent shownFlatConsistent = new ShownFlatConsistent();
+                System.out.println("Введите номер квартиры");
+                int num = scanner.nextInt();
+                shownFlatConsistent.mainCommand(num, list);
+                break;
+            case 7:
+                ShownRange shownRange = new ShownRange();
+                System.out.println("Введите нижнюю границу");
+                double min = scanner.nextDouble();
+                System.out.println("Введите верхнюю границу");
+                double max = scanner.nextDouble();
+                shownRange.mainCommand(min, max, list);
+                break;
+            case 3:
+                ShowAvailableFlats showAvailableFlats = new ShowAvailableFlats();
+                showAvailableFlats.mainCommand(list);
+                break;
+            case 4:
+                ShowNotAvailableFlats showNotAvailableFlats = new ShowNotAvailableFlats();
+                showNotAvailableFlats.mainCommand(list);
+                break;
+            case 5:
+                BuyTheFlat buyTheFlat = new BuyTheFlat();
+                System.out.println("Enter the flat's number: ");
+                int n = scanner.nextInt();
+                buyTheFlat.mainCommand(n, list);
+                System.out.println("You have already bought flat number: " + n);
+                break;
         }
-        return chooseCommand;
     }
-
-
 }
--- src/Main.java
+++ src/Main.java
@@ -4,62 +4,15 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
         int commandId = 0;
         Novostroika novostroika = new Novostroika(new ArrayList<>());
         PrintListOfCommands printListOfCommands = new PrintListOfCommands();
-        int maxCommandId = printListOfCommands.mainCommand2(novostroika.list);
-        PrintListOfFlats printListOfFlats = new PrintListOfFlats();
-        ShowAvailableFlats showAvailableFlats = new ShowAvailableFlats();
-        ShowNotAvailableFlats showNotAvailableFlats = new ShowNotAvailableFlats();
-        ShownFlatConsistent shownFlatConsistent = new ShownFlatConsistent();
-        BuyTheFlat buyTheFlat = new BuyTheFlat();
-        ShownRange shownRange = new ShownRange();
-
-
-
+        int a = printListOfCommands.mainCommand2(novostroika.list);
 
         while (commandId != -1) {
-            if (scanner.hasNextInt()) {
-                commandId = scanner.nextInt();
-                if (commandId == 0) {
-                    printListOfCommands.mainCommand2(novostroika.list);
-                } else if (commandId > maxCommandId || commandId < -1) {
-                    System.out.println("Неверный номер команды");
-                } else if (commandId == 1) {
-                    printListOfFlats.mainCommand(novostroika.list);
-                } else if (commandId == 6) {
-                    System.out.println("Введите номер квартиры");
-                    int num = scanner.nextInt();
-                    shownFlatConsistent.mainCommand(num, novostroika.list);
-                } else if (commandId == 7) {
-                    System.out.println("Введите нижнюю границу");
-                    if (scanner.hasNextDouble()) {
-                        double min = scanner.nextDouble();
-                        System.out.println("Введите верхнюю границу");
-                        if (scanner.hasNextDouble()) {
-                            double max = scanner.nextDouble();
-                            shownRange.mainCommand(min, max, novostroika.list);
-                        }
-                    }
-                } else if (commandId == 3) {
-                    showAvailableFlats.mainCommand(novostroika.list);
-                } else if (commandId == 4) {
-                    showNotAvailableFlats.mainCommand(novostroika.list);
-                } else if (commandId == 5) {
-                    System.out.println("Enter the flat's number: ");
-                    if (scanner.hasNextInt()) {
-                        int num = scanner.nextInt();
-                        buyTheFlat.mainCommand(num, novostroika.list);
-                        System.out.println("You have already bought flat number: " + num);
-                    }
-                }
-            } else {
-                scanner.next();
-                System.out.println("Irregular enter. Enter the number of command");
-            }
+            commandId = scanner.nextInt();
+            FactoryMethod.getCommand(commandId);
         }
     }
-
 }
